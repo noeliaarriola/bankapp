e9f472afe97a42d9c72814bf5c1ae11941f826e2
Noelia Arriola <noelia.arriola.1@gmail.com>
2023-06-23 23:21:03 -0300

    Se agrega el proyecto

---
+++ src/components/CTA.jsx
@@ -0,0 +1,22 @@
+import styles from '../style';
+import Button from './Button';
+
+const CTA = () => (
+  <section className={`${styles.flexCenter} ${styles.marginY} 
+   ${styles.padding} sm:flex-row flex-col bg-black-gradient-2
+    rounded-[20px] box-shadow`}>
+    
+   <div className="flex-1 flex flex-col">
+
+    <h2 className={styles.heading2}>Let´s try our service now!</h2>
+    <p className={styles.paragraph}>Everything you need to accept card payments and 
+      grow your business anywhere on the planet.</p>
+      </div>
+
+    <div>
+     <Button />
+    </div>
+  </section>
+)
+
+export default CTA
