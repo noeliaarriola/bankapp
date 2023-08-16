5d23aeb43e631bfaac0c8a97d6553f060b9073cf
noeliaarriola <noelia.arriola.1@gmail.com>
2023-08-16 20:02:19 -0300

    borre la linea, hola

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ const App = () => (
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
             <Navbar />
-
         </div>
       </div>
 
